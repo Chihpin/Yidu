bf5349d3165f6197e7113c2cae039d448627acc9
shinpa.you <silent0608@gmail.com>
2014-09-25 22:59:25 +0900

    整理Import

--- src/main/java/org/yidu/novel/utils/MailUtils.java
+++ src/main/java/org/yidu/novel/utils/MailUtils.java
@@ -4,14 +4,11 @@ import java.util.Properties;
 
 import javax.mail.Message;
 import javax.mail.MessagingException;
-import javax.mail.Multipart;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
