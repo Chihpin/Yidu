87130ecb06ead3553242cf80d0802ce583d37d3e
shinpa.you <silent0608@gmail.com>
2014-12-16 23:02:34 +0900

    增加分段阅读的章节限制

--- src/main/java/org/yidu/novel/service/impl/ChapterServiceImpl.java
+++ src/main/java/org/yidu/novel/service/impl/ChapterServiceImpl.java
@@ -141,7 +141,7 @@ public class ChapterServiceImpl extends HibernateSupportServiceImpl implements C
     public List<TChapter> getChapterInSegement(Integer articleno, Integer chapterno, Integer toChapterno) {
         StringBuffer hql = new StringBuffer("FROM TChapter "
                 + "WHERE deleteflag=false and articleno = ? and chapterno>=? and chapterno<=? "
-                + "order by chapterno asc");
+                + "order by chapterno asc limit 30 ");
         List<Object> params = new ArrayList<Object>();
         // 追加小说号条件
         params.add(articleno);
